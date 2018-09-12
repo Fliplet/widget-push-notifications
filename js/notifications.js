39431183e7f62bc93fd189e3f176cf1f05715fa9
Nicholas Valbusa <squallstar@gmail.com>
2018-09-12 15:53:05 +0200

    initial work for in-app notifications

--- js/notifications.js
+++ js/notifications.js
@@ -32,45 +32,56 @@ Fliplet.Widget.register('PushNotifications', function () {
     return Fliplet.User.subscribe(data);
   }
 
-  function handleForegroundNotification(data) {
-    $('#notificationContainer').remove();
-    $('body').append(Fliplet.Native.Templates.InAppNotification);
-
-    $('#appName').text(Fliplet.Env.get('appName'));
-    $('#notificationTitle').text(data.title);
-    $('#notificationBody').text(data.message);
-
-    if (Fliplet.Env.get('pageId') === parseInt(data.additionalData.data.page)) {
-      $('#btnNotificationNavigate').addClass('hidden');
-    } else {
-      $('#btnNotificationNavigate').on('click', function () {
-        handleNotificationPayload(data.additionalData.data);
-      });
+  function handleNotificationPayload(data) {
+    if (!data || !data.page) {
+      return;
     }
 
-    setTimeout(function () {
-      $('#notificationContainer').remove();
-    }, 3000);
-  }
+    var appPages = Fliplet.Env.get('appPages');
 
-  function handleNotificationPayload(data) {
-    if (data && data.page) {
-      var appPages = Fliplet.Env.get('appPages');
+    if (Array.isArray(appPages) && appPages.length) {
+      var page = appPages.filter(function(page) { return page.id === parseInt(data.page);});
 
-      if (Array.isArray(appPages) && appPages.length) {
-        var page = appPages.filter(function(page) { return page.id === parseInt(data.page);});
-
-        if (!page.length || Fliplet.Env.get('pageId') === parseInt(data.page)) {
-          Fliplet.Native.Updates.checkForUpdates(Fliplet.Env.get('appId'), true, null, data);
-          return;
-        }
-        else {
-          Fliplet.Storage.set('fl_notification_update', data).then(function () {
-            Fliplet.Navigate.to(data);
-          });
-        }
+      if (!page.length || Fliplet.Env.get('pageId') === parseInt(data.page)) {
+        Fliplet.Native.Updates.checkForUpdates(Fliplet.Env.get('appId'), true, null, data);
+        return;
+      }
+      else {
+        Fliplet.Storage.set('fl_notification_update', data).then(function () {
+          Fliplet.Navigate.to(data);
+        });
       }
     }
+  }
+
+  function bindLocalNotificationsClick() {
+    if (!Fliplet.Env.is('native')) {
+      return; // only native devices
+    }
+
+    if (typeof cordova.plugins.notification === 'undefined') {
+      return; // do nothing if native app hasn't got the plugin installed
+    }
+
+    cordova.plugins.notification.local.on('click', function () {
+      console.log('Clicked notification', this, arguments);
+      handleNotificationPayload();
+    }, this);
+
+    if (cordova.plugins.notification.local.launchDetails) {
+      console.log('Launch details', launchDetails);
+    }
+  }
+
+  function handleForegroundNotification(data) {
+    Fliplet.Navigator.Notifications.schedule({
+      title: data.title,
+      text: data.message,
+      data: data.additionalData && data.additionalData.data
+    }, function () {
+      // notification has been scheduled
+      console.log('Notification scheduled');
+    }, this, { skipPermission: true });
   }
 
   function ask() {
@@ -185,6 +196,8 @@ Fliplet.Widget.register('PushNotifications', function () {
               }
             });
           });
+
+          bindLocalNotificationsClick();
         }
       }
 
