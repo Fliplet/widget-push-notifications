61ffa8080b2a8cc6199a5c0f2ca6d5e886f90e79
Tony Wu <tonytlwu@users.noreply.github.com>
2019-01-24 19:16:51 +0200

    Update js/notifications.js
    Co-Authored-By: angelnikolov <darkysharky@gmail.com>

--- js/notifications.js
+++ js/notifications.js
@@ -40,7 +40,9 @@ Fliplet.Widget.register('PushNotifications', function () {
     var appPages = Fliplet.Env.get('appPages');
 
     if (Array.isArray(appPages) && appPages.length) {
-      var page = appPages.filter(function(page) { return page.masterPageId === parseInt(data.page) || page.id === parseInt(data.page);});
+      var page = appPages.filter(function(page) {
+        return page.masterPageId === parseInt(data.page, 10) || page.id === parseInt(data.page, 10);
+      });
 
       if (!page.length || Fliplet.Env.get('pageId') === parseInt(data.page)) {
         Fliplet.Native.Updates.checkForUpdates(Fliplet.Env.get('appId'), true, null, data);
