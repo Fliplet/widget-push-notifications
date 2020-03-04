3f6d0dc5f5e8d2d5a38ad28681bdcc369587016f
Tony Wu <twu@fliplet.com>
2020-03-04 22:01:20 +0000

    Adds link to notification support article

--- src/components/NotificationForm.vue
+++ src/components/NotificationForm.vue
@@ -93,7 +93,7 @@
             </template>
             <div class="col-xs-12 filter-summary">
               <div class="col-xs-8">
-                <p v-if="audience !== 'sessions'"><a href="#" class="btn btn-default" @click.prevent="addFilter"><i class="fa fa-fw fa-plus"></i> Add filter</a> <a href="#" target="_blank" class="filter-help">How to add data for filtering users</a></p>
+                <p v-if="audience !== 'sessions'"><a href="#" class="btn btn-default" @click.prevent="addFilter"><i class="fa fa-fw fa-plus"></i> Add filter</a> <a href="https://help.fliplet.com/article/177-notifications" target="_blank" class="filter-help">How to add data for filtering users</a></p>
               </div>
               <div class="col-xs-4 text-right">
                 <p>
